98ac0d1024e52da7fcf95cc09a5a6f833ac3405f
Frezer <frezertade@gmail.com>
2017-06-21 17:19:36 +0300

    good start nice

---
+++ module/WedeChurch/src/Entities/Church.php
@@ -0,0 +1,365 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fre
+ * Date: 6/21/17
+ * Time: 8:53 AM
+ */
+
+namespace WedeChurch\Entities;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="church")
+ */
+class Church extends BaseTable
+{
+    /**
+     * @ORM\Idd
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(name="name", type="string", unique=false, nullable=false)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(name="country", type="string", unique=false, nullable=false)
+     */
+    protected $country;
+    /**
+     * @ORM\Column(name="cities", type="string", unique=false, nullable=false)
+     */
+    protected $cities;
+    /**
+     * @ORM\Column(name="state", type="string", unique=false, nullable=false)
+     */
+    protected $state;
+    /**
+     * @ORM\Column(name="location", type="string", unique=false, nullable=false)
+     */
+    protected $location;
+    /**
+     * @ORM\Column(name="latitude", type="string", unique=false, nullable=false)
+     */
+    protected $latitude;
+    /**
+     * @ORM\Column(name="longitude", type="string", unique=false, nullable=false)
+     */
+    protected $longitude;
+    /**
+     * @ORM\Column(name="phone", type="string", unique=false, nullable=false)
+     */
+    protected $phone;
+    /**
+     * @ORM\Column(name="WebUrl", type="string", unique=false, nullable=false)
+     */
+    protected $WebUrl;
+    /**
+     * @ORM\Column(name="denomination_id", type="string", unique=false, nullable=false)
+     */
+    protected $denomination_id;
+    /**
+     * @ORM\Column(name="banner", type="string", unique=false, nullable=false)
+     */
+    protected $banner;
+    /**
+     * @ORM\Column(name="description", type="string", unique=false, nullable=false)
+     */
+    protected $description;
+    /**
+     * @ORM\Column(name="logo", type="string", unique=false, nullable=false)
+     */
+    protected $logo;
+    /**
+     * @ORM\Column(name="parent_church_id", type="string", unique=false, nullable=false)
+     */
+    protected $parent_church_id;
+
+    /**
+     * @return mixed
+     */
+    public function getisDeleted()
+    {
+        return $this->isDeleted;
+    }
+
+    /**
+     * @param mixed $isDeleted
+     */
+    public function setIsDeleted($isDeleted)
+    {
+        $this->isDeleted = $isDeleted;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCities()
+    {
+        return $this->cities;
+    }
+
+    /**
+     * @param mixed $cities
+     */
+    public function setCities($cities)
+    {
+        $this->cities = $cities;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param mixed $state
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param mixed $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param mixed $latitude
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param mixed $longitude
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWebUrl()
+    {
+        return $this->WebUrl;
+    }
+
+    /**
+     * @param mixed $WebUrl
+     */
+    public function setWebUrl($WebUrl)
+    {
+        $this->WebUrl = $WebUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDenominationId()
+    {
+        return $this->denomination_id;
+    }
+
+    /**
+     * @param mixed $denomination_id
+     */
+    public function setDenominationId($denomination_id)
+    {
+        $this->denomination_id = $denomination_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBanner()
+    {
+        return $this->banner;
+    }
+
+    /**
+     * @param mixed $banner
+     */
+    public function setBanner($banner)
+    {
+        $this->banner = $banner;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    /**
+     * @param mixed $logo
+     */
+    public function setLogo($logo)
+    {
+        $this->logo = $logo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParentChurchId()
+    {
+        return $this->parent_church_id;
+    }
+
+    /**
+     * @param mixed $parent_church_id
+     */
+    public function setParentChurchId($parent_church_id)
+    {
+        $this->parent_church_id = $parent_church_id;
+    }
+
+    public function getArray(){
+        return array(
+            'id'=>$this->getId(),
+            'name'=>$this->getName(),
+            'country' => $this->getCountry(),
+            'cities'=>$this->getCities(),
+            'location'=>$this->getLocation(),
+            'latitude'=>$this->getLatitude(),
+            'longitude'=>$this->getLongitude(),
+            'Phone'=>$this->getPhone(),
+            'banner'=>$this->getBanner(),
+            'WebUrl'=>$this->getLongitude(),
+            'denomination_id'=>$this->getDenominationId(),
+            'description'=>$this->getDescription(),
+            'logo'=>$this->getLogo(),
+            'parent_church_id'=>$this->getParentChurchId(),
+            'state' => $this->getState(),
+            'is_deleted'=>$this->getIsDeleted(),
+            'is_active'=>$this->getIsActive(),
+            'updated_by'=>$this->getUpdatedBy()->getFullName(),
+            'updated_date'=>$this->getUpdatedDate(),
+            'created_by'=>$this->getCreatedBy()->getFullName(),
+            'created_date'=>$this->getCreatedDate(),
+        );
+    }
+
+
+
+}
---
+++ module/WedeChurch/src/Entities/Event.php
@@ -0,0 +1,289 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fre
+ * Date: 6/21/17
+ * Time: 9:20 AM
+ */
+
+namespace WedeChurch\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="event")
+ */
+class Event extends BaseTable
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(name="name", type="string", unique=false, nullable=false)
+     */
+    protected $name;
+    /**
+     * One Product has Many Features.
+     * @ORM\ManyToOne(targetEntity="church")
+     * @ORM\JoinColumn(name="church_id", referencedColumnName="id")
+     * @var church $church_id
+     */
+    protected $church_id;
+    /**
+     * @ORM\Column(name="location", type="string", unique=false, nullable=false)
+     */
+    protected $location;
+    /**
+     * @ORM\Column(name="latitude", type="string", unique=false, nullable=false)
+     */
+    protected $latitude;
+    /**
+     * @ORM\Column(name="longitude", type="string", unique=false, nullable=false)
+     */
+    protected $longitude;
+    /**
+     * One Product has Many Features.
+     * @ORM\ManyToOne(targetEntity="event_category")
+     * @ORM\JoinColumn(name="event_category_id", referencedColumnName="id")
+     * @var event_category $event_category_id
+     */
+    protected $event_category_id;
+    /**
+     * @ORM\Column(name="banner", type="string", unique=false, nullable=false)
+     */
+    protected $banner;
+    /**
+     * @ORM\Column(name="description", type="string", unique=false, nullable=false)
+     */
+    protected $description;
+    /**
+     * @ORM\Column(name="contact_info", type="string", unique=false, nullable=false)
+     */
+    protected $contact_info;
+    /**
+     * @ORM\Column(name="start_date", type="datetime", unique=false, nullable=false)
+     */
+    protected $start_date;
+    /**
+     * @ORM\Column(name="end_date", type="datetime", unique=false, nullable=false)
+     */
+    protected $end_date;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return church
+     */
+    public function getChurchId()
+    {
+        return $this->church_id;
+    }
+
+    /**
+     * @param church $church_id
+     */
+    public function setChurchId($church_id)
+    {
+        $this->church_id = $church_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param mixed $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param mixed $latitude
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param mixed $longitude
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @return event_category
+     */
+    public function getEventCatagoryId()
+    {
+        return $this->event_catagory_id;
+    }
+
+    /**
+     * @param event_category $event_category_id
+     */
+    public function setEventCatagoryId($event_catagory_id)
+    {
+        $this->event_catagory_id = $event_catagory_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBanner()
+    {
+        return $this->banner;
+    }
+
+    /**
+     * @param mixed $banner
+     */
+    public function setBanner($banner)
+    {
+        $this->banner = $banner;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContactInfo()
+    {
+        return $this->contact_info;
+    }
+
+    /**
+     * @param mixed $contact_info
+     */
+    public function setContactInfo($contact_info)
+    {
+        $this->contact_info = $contact_info;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStartDate()
+    {
+        return $this->start_date;
+    }
+
+    /**
+     * @param mixed $start_date
+     */
+    public function setStartDate($start_date)
+    {
+        $this->start_date = $start_date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndDate()
+    {
+        return $this->end_date;
+    }
+
+    /**
+     * @param mixed $end_date
+     */
+    public function setEndDate($end_date)
+    {
+        $this->end_date = $end_date;
+    }
+
+    public function getArray(){
+        return array(
+            'id'=>$this->getId(),
+            'church_id' =>$this->getChurchId(),
+            'location' => $this ->getLocation(),
+            'latitude' => $this ->getLatitude(),
+            'longitude' => $this ->getLongitude(),
+            'event_category_id'=>$this->getEventCatagoryId(),
+            'banner'=>$this->getBanner(),
+            'description'=>$this->getDescription(),
+            'contact_info'=>$this->getContactInfo(),
+            'start_date'=>$this->getStartDate(),
+            'end_date'=>$this->getEndDate(),
+            'is_deleted'=>$this->getIsDeleted(),
+            'is_active'=>$this->getIsActive(),
+            'updated_by'=>$this->getUpdatedBy()->getFullName(),
+            'updated_date'=>$this->getUpdatedDate(),
+            'created_by'=>$this->getCreatedBy()->getFullName(),
+            'created_date'=>$this->getCreatedDate(),
+        );
+    }
+
+}
