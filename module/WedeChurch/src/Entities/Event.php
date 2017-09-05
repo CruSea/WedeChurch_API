7ba7fe79de7fce6250d5b63333cf89b8a62efed4
frezertade <frezertade@gmail.com>
2017-09-05 16:14:50 +0300

    event fix

--- module/WedeChurch/src/Entities/Event.php
+++ module/WedeChurch/src/Entities/Event.php
@@ -30,7 +30,7 @@ class Event extends BaseTable
      * One Product has Many Features.
      * @ORM\ManyToOne(targetEntity="Church")
      * @ORM\JoinColumn(name="church_id", referencedColumnName="id")
-     * @var church $id
+     * @var church $church
      */
     protected $church_id;
     /**
