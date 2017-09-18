d6dfb254e3a90b2ae4306a2b3f7fb22c71c96569
frezertade <frezertade@gmail.com>
2017-09-18 15:18:54 +0300

    event category updated fix

--- module/WedeChurch/src/Entities/Event_category.php
+++ module/WedeChurch/src/Entities/Event_category.php
@@ -64,9 +64,9 @@ class Event_category extends BaseTable
             'name'=>$this->getName(),
             'is_deleted'=>$this->getIsDeleted(),
             'is_active'=>$this->getIsActive(),
-            'updated_by'=>$this->getUpdatedBy()->getFullName(),
+            'updated_by'=>$this->getUpdatedBy()->getId(),
             'updated_date'=>$this->getUpdatedDate(),
-            'created_by'=>$this->getCreatedBy()->getFullName(),
+            'created_by'=>$this->getCreatedBy()->getId(),
             'created_date'=>$this->getCreatedDate(),
         );
     }
